a490752fbe7a9316642f384a5592e15b5d67c3e4
shamsuttabriz <shamsuttabriz.md@gmail.com>
2024-09-23 04:30:58 +0600

    Toggle page functionality added

--- js/togglePage.js
+++ js/togglePage.js
@@ -1,3 +1,7 @@
-document.getElementById('btn-blog').addEventListener('click', function () {
-    window.location.href = 'blog.html';
+document.getElementById('btn-blog')?.addEventListener('click', function () {
+    window.location.href = '/blog.html';
+})
+
+document.getElementById('btn-home')?.addEventListener('click', function () {
+    window.location.href = '/';
 })
